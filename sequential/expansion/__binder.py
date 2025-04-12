8fa2b5d2c24161dbde79eaa67212421978566eb3
Ryan O'Dea <70209371+ryan-odea@users.noreply.github.com>
2025-04-12 19:55:01 -0400

    Update __binder.py

--- sequential/expansion/__binder.py
+++ sequential/expansion/__binder.py
@@ -1,6 +1,8 @@
 import polars as pl
 
-def __binder(DT, data, id_col, time_col, cols, baseline_indicator, squared_indicator):
+def __binder(DT, data, id_col, time_col,
+            eligible_col, excused_col0, excused_col1, cols, 
+            baseline_indicator, squared_indicator):
     """
     Internal function to bind data to the map created by __mapper
     """
@@ -9,6 +11,7 @@ def __binder(DT, data, id_col, time_col, cols, baseline_indicator, squared_indic
                 time_col,
                 f'{time_col}{squared_indicator}',
                 'tx_lag'}
+    cols = cols.union({eligible_col, excused_col0, excused_col1})
    
     regular = {col for col in cols if not (baseline_indicator in col or squared_indicator in col) and col not in excluded}
     baseline = {col for col in cols if baseline_indicator in col and col not in excluded}
